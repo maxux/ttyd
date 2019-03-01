d71c7052cb0aee11cd665ec9ab5eafcc7bb78fe8
Maxime Daniel <root@maxux.net>
2019-03-01 04:23:00 +0100

    Complete rewrite of how HTTP routing is done, implementing API
    New routing:
    - One function per route
    - New endpoints
    - PID is reached via /pid/[id], / is not used anymore
    
    API Routing:
    - /api/processes: list processes
    - /api/process/start: create a new process
    - /api/process/stop: stop a process by it's pid
    - /api/process/logs: get current logs buffer of process

--- src/http.c
+++ src/http.c
@@ -1,8 +1,20 @@
 #include <string.h>
 #include <libwebsockets.h>
+#include <json.h>
+#include <signal.h>
 
 #include "server.h"
 #include "html.h"
+#include "utils.h"
+
+struct callback_response {
+    struct lws *wsi;
+    struct pss_http *pss;
+    unsigned char *buffer;
+    unsigned char *p;
+    unsigned char *end;
+};
+
 
 int
 check_auth(struct lws *wsi) {
@@ -50,20 +62,301 @@ check_auth(struct lws *wsi) {
     return -1;
 }
 
-int
-callback_http(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len) {
+static int http_response(struct callback_response *r, char *ctype, size_t length, char *buffer) {
+    if(lws_add_http_header_status(r->wsi, HTTP_STATUS_OK, &r->p, r->end))
+        return 1;
+
+    if(lws_add_http_header_by_token(r->wsi, WSI_TOKEN_HTTP_CONTENT_TYPE, ctype, strlen(ctype), &r->p, r->end))
+        return 1;
+
+    if(lws_add_http_header_content_length(r->wsi, length, &r->p, r->end))
+        return 1;
+
+    if(lws_finalize_http_header(r->wsi, &r->p, r->end))
+        return 1;
+
+    if(lws_write(r->wsi, r->buffer + LWS_PRE, r->p - (r->buffer + LWS_PRE), LWS_WRITE_HTTP_HEADERS) < 0)
+        return 1;
+
+    if(!buffer)
+        return 0;
+
+    r->pss->buffer = r->pss->ptr = strdup(buffer);
+    r->pss->len = length;
+    lws_callback_on_writable(r->wsi);
+
+    return 0;
+}
+
+//
+// json status
+//
+char *http_response_json_ok() {
+    struct json_object *root = json_object_new_object();
+    json_object_object_add(root, "status", json_object_new_string("success"));
+
+    char *jsondumps = strdup(json_object_to_json_string(root));
+    json_object_put(root);
+
+    return jsondumps;
+}
+
+char *http_response_json_error(char *msg) {
+    struct json_object *root = json_object_new_object();
+    json_object_object_add(root, "status", json_object_new_string("error"));
+    json_object_object_add(root, "reason", json_object_new_string(msg));
+
+    char *jsondumps = strdup(json_object_to_json_string(root));
+    json_object_put(root);
+
+    return jsondumps;
+}
+
+//
+// methods
+//
+static inline int http_method_is_get(struct lws *wsi) {
+    return lws_hdr_total_length(wsi, WSI_TOKEN_GET_URI);
+}
+
+static inline int http_method_is_post(struct lws *wsi) {
+    return lws_hdr_total_length(wsi, WSI_TOKEN_POST_URI);
+}
+
+//
+// routing
+//
+static int routing_get_root(struct callback_response *r) {
+    lws_return_http_status(r->wsi, HTTP_STATUS_NOT_FOUND, NULL);
+    return 0;
+}
+
+static int routing_get_pid(struct callback_response *r, char *pid) {
+    if(strlen(pid) == 0) {
+        lwsl_err("pid not defined\n");
+        lws_return_http_status(r->wsi, HTTP_STATUS_NOT_FOUND, NULL);
+        return 1;
+    }
+
+    lwsl_notice("requesting to attach pid <%s>\n", pid);
+
+    // checking for pid validity, this is actually not an error
+    // the client will not be able to connect later via the websocket
+    // anyway, but we can at least ensure it's an integer...
+    if(atoi(pid) < 1) {
+        lwsl_err("invalid pid\n");
+        lws_return_http_status(r->wsi, HTTP_STATUS_NOT_FOUND, NULL);
+        return 1;
+    }
+
+    if(server->index == NULL)
+        return http_response(r, "text/html", index_html_len, index_html);
+
+    int n = lws_serve_http_file(r->wsi, server->index, "text/html", NULL, 0);
+    if(n < 0 || (n > 0 && lws_http_transaction_completed(r->wsi)))
+        return 1;
+}
+
+static int routing_get_auth_token(struct callback_response *r) {
+    char buf[512];
+
+    size_t n = server->credential != NULL ? sprintf(buf, "var tty_auth_token = '%s';", server->credential) : 0;
+    return http_response(r, "application/javascript", n, n ? buf : NULL);
+}
+
+// api
+
+static int routing_get_api_processes(struct callback_response *r) {
+    struct json_object *root = json_object_new_object();
+    struct json_object *processes = json_object_new_array();
+
+    struct tty_process *proc;
+
+    LIST_FOREACH(proc, &server->processes, list) {
+        struct json_object *process = json_object_new_object();
+
+        json_object_object_add(process, "pid", json_object_new_int64(proc->pid));
+        json_object_object_add(process, "command", json_object_new_string(proc->command));
+        json_object_object_add(process, "running", json_object_new_boolean(proc->running));
+
+        json_object_array_add(processes, process);
+    }
+
+    json_object_object_add(root, "processes", processes);
+
+    char *jsondumps = strdup(json_object_to_json_string(root));
+    json_object_put(root);
+
+    int value = http_response(r, "application/json", strlen(jsondumps), jsondumps);
+    free(jsondumps);
+
+    return value;
+
+}
+
+static int routing_get_api_process_start(struct callback_response *r) {
+    char cmdline[512];
+    char *binary = NULL;
+    char **argv = NULL;
+    int argc = 0;
+
+    while(lws_hdr_copy_fragment(r->wsi, cmdline, sizeof(cmdline), WSI_TOKEN_HTTP_URI_ARGS, argc) > 0) {
+        if(strncmp(cmdline, "arg[]=", 6) == 0)
+            argc += 1;
+    }
+
+    if(argc == 0) {
+        char *status = http_response_json_error("missing cmdline");
+
+        int value = http_response(r, "application/json", strlen(status), status);
+        free(status);
+
+        return value;
+    }
+
+    argv = xmalloc(sizeof(char *) * argc);
+    int j = 0;
+
+    for(int i = 0; ; i++) {
+        if(lws_hdr_copy_fragment(r->wsi, cmdline, sizeof(cmdline), WSI_TOKEN_HTTP_URI_ARGS, i) < 0)
+            break;
+
+        if(strncmp(cmdline, "arg[]=", 6) == 0) {
+            argv[j] = strdup(cmdline + 6);
+            j += 1;
+        }
+    }
+
+    lwsl_warn("starting process: %s [with %d args]\n", argv[0], argc - 1);
+    struct tty_process *proc = tty_server_attach_process(server, argc, argv);
+
+    // FIXME.
+    while(!proc->pid)
+        usleep(1000000);
+
+    struct json_object *root = json_object_new_object();
+    json_object_object_add(root, "status", json_object_new_string("success"));
+    json_object_object_add(root, "pid", json_object_new_int64(proc->pid));
+
+    char *jsondumps = strdup(json_object_to_json_string(root));
+    json_object_put(root);
+
+    int value = http_response(r, "application/json", strlen(jsondumps), jsondumps);
+    free(jsondumps);
+
+    return value;
+}
+
+static int routing_get_api_process_stop(struct callback_response *r) {
+    const char *ppid;
+    char pid[32];
+
+    if(!(ppid = lws_get_urlarg_by_name(r->wsi, "pid=", pid, sizeof(pid)))) {
+        char *status = http_response_json_error("missing pid");
+        int value = http_response(r, "application/json", strlen(status), status);
+        free(status);
+        return value;
+    }
+
+    int ipid = atoi(ppid);
+    lwsl_warn("requesing stopping process: %d\n", ipid);
+
+    // looking for and killing processes
+    struct tty_process *process;
+    if(!(process = process_getby_pid(ipid, 1))) {
+        char *status = http_response_json_error("invalid pid");
+        int value = http_response(r, "application/json", strlen(status), status);
+        free(status);
+        return value;
+    }
+
+    lwsl_warn("killing process: %d\n", process->pid);
+    kill(process->pid, SIGTERM);
+    process->running = false;
+
+    char *status = http_response_json_ok();
+    int value = http_response(r, "application/json", strlen(status), status);
+    free(status);
+
+    return value;
+}
+
+static int routing_get_api_process_logs(struct callback_response *r) {
+    const char *ppid;
+    char pid[32];
+
+    if(!(ppid = lws_get_urlarg_by_name(r->wsi, "pid=", pid, sizeof(pid)))) {
+        char *status = http_response_json_error("missing pid");
+        int value = http_response(r, "application/json", strlen(status), status);
+        free(status);
+        return value;
+    }
+
+    int ipid = atoi(ppid);
+    lwsl_warn("requesing stopping process: %d\n", ipid);
+
+    // killing processes
+    struct tty_process *process;
+    if(!(process = process_getby_pid(ipid, 1))) {
+        char *status = http_response_json_error("invalid pid");
+        int value = http_response(r, "application/json", strlen(status), status);
+        free(status);
+        return value;
+    }
+
+    // fetching logs.
+    buffer_t *logs = circular_get(process->logs, 0);
+
+    int value = http_response(r, "text/plain", logs->length, logs->buffer);
+    buffer_free(logs);
+
+    return value;
+}
+
+
+//
+// callback
+//
+int callback_http(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len) {
     struct pss_http *pss = (struct pss_http *) user;
     unsigned char buffer[4096 + LWS_PRE], *p, *end;
     char buf[256], name[100], rip[50];
 
+    struct callback_response r = {
+        .wsi = wsi,
+        .pss = pss,
+        .buffer = buffer,
+        .p = p,
+        .end = end,
+    };
+
     switch (reason) {
-        case LWS_CALLBACK_HTTP:
-            // only GET method is allowed
-            if (!lws_hdr_total_length(wsi, WSI_TOKEN_GET_URI) || len < 1) {
+        case LWS_CALLBACK_HTTP: {
+            if(len < 1) {
                 lws_return_http_status(wsi, HTTP_STATUS_BAD_REQUEST, NULL);
                 goto try_to_reuse;
             }
 
+            // initialize context
+            r.p = r.buffer + LWS_PRE;
+            r.end = r.p + sizeof(buffer) - LWS_PRE;
+
+            if(http_method_is_get(wsi))
+                goto routing_get;
+
+            /*
+            if(http_method_is_post(wsi))
+                goto routing_post;
+            */
+
+            // method not allowed
+            lws_return_http_status(wsi, HTTP_STATUS_BAD_REQUEST, NULL);
+            goto try_to_reuse;
+
+            //
+            // GET
+            //
+routing_get:
             snprintf(pss->path, sizeof(pss->path), "%s", (const char *)in);
             lws_get_peer_addresses(wsi, lws_get_socket_fd(wsi), name, sizeof(name), rip, sizeof(rip));
             lwsl_notice("HTTP %s - %s (%s)\n", (char *) in, rip, name);
@@ -78,72 +371,33 @@ callback_http(struct lws *wsi, enum lws_callback_reasons reason, void *user, voi
                     return 1;
             }
 
-            p = buffer + LWS_PRE;
-            end = p + sizeof(buffer) - LWS_PRE;
 
-            if (strncmp(pss->path, "/auth_token.js", 14) == 0) {
-                size_t n = server->credential != NULL ? sprintf(buf, "var tty_auth_token = '%s';", server->credential) : 0;
+            if(strcmp(pss->path, "/") == 0)
+                return routing_get_root(&r);
 
-                if (lws_add_http_header_status(wsi, HTTP_STATUS_OK, &p, end))
-                    return 1;
-                if (lws_add_http_header_by_token(wsi,
-                                                 WSI_TOKEN_HTTP_CONTENT_TYPE,
-                                                 (unsigned char *) "application/javascript",
-                                                 22, &p, end))
-                    return 1;
-                if (lws_add_http_header_content_length(wsi, (unsigned long) n, &p, end))
-                    return 1;
-                if (lws_finalize_http_header(wsi, &p, end))
-                    return 1;
-                if (lws_write(wsi, buffer + LWS_PRE, p - (buffer + LWS_PRE), LWS_WRITE_HTTP_HEADERS) < 0)
-                    return 1;
-#if LWS_LIBRARY_VERSION_MAJOR < 3
-                if (n > 0 && lws_write_http(wsi, buf, n) < 0)
-                    return 1;
-                goto try_to_reuse;
-#else
-                if (n > 0) {
-                    pss->buffer = pss->ptr = strdup(buf);
-                    pss->len = n;
-                    lws_callback_on_writable(wsi);
-                }
-                return 0;
-#endif
-            }
+            if(strncmp(pss->path, "/pid/", 5) == 0)
+                return routing_get_pid(&r, pss->path + 5);
 
-            if (strcmp(pss->path, "/") != 0) {
-                lws_return_http_status(wsi, HTTP_STATUS_NOT_FOUND, NULL);
-                goto try_to_reuse;
-            }
+            if(strncmp(pss->path, "/auth_token.js", 14) == 0)
+                return routing_get_auth_token(&r);
 
-            const char* content_type = "text/html";
-            if (server->index != NULL) {
-                int n = lws_serve_http_file(wsi, server->index, content_type, NULL, 0);
-                if (n < 0 || (n > 0 && lws_http_transaction_completed(wsi)))
-                    return 1;
-            } else {
-                if (lws_add_http_header_status(wsi, HTTP_STATUS_OK, &p, end))
-                    return 1;
-                if (lws_add_http_header_by_token(wsi, WSI_TOKEN_HTTP_CONTENT_TYPE, (const unsigned char *) content_type, 9, &p, end))
-                    return 1;
-                if (lws_add_http_header_content_length(wsi, (unsigned long) index_html_len, &p, end))
-                    return 1;
-                if (lws_finalize_http_header(wsi, &p, end))
-                    return 1;
-                if (lws_write(wsi, buffer + LWS_PRE, p - (buffer + LWS_PRE), LWS_WRITE_HTTP_HEADERS) < 0)
-                    return 1;
-#if LWS_LIBRARY_VERSION_MAJOR < 3
-                if (lws_write_http(wsi, index_html, index_html_len) < 0)
-                    return 1;
-                goto try_to_reuse;
-#else
-                pss->buffer = pss->ptr = (char *) index_html;
-                pss->len = index_html_len;
-                lws_callback_on_writable(wsi);
-                return 0;
-#endif
-            }
-            break;
+            if(strcmp(pss->path, "/api/processes") == 0)
+                return routing_get_api_processes(&r);
+
+            if(strcmp(pss->path, "/api/process/start") == 0)
+                return routing_get_api_process_start(&r);
+
+            if(strcmp(pss->path, "/api/process/stop") == 0)
+                return routing_get_api_process_stop(&r);
+
+            if(strcmp(pss->path, "/api/process/logs") == 0)
+                return routing_get_api_process_logs(&r);
+
+
+            // anything else, not found
+            lws_return_http_status(wsi, HTTP_STATUS_NOT_FOUND, NULL);
+            goto try_to_reuse;
+        }
 
         case LWS_CALLBACK_HTTP_WRITEABLE:
             if (pss->len <= 0)
