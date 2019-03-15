fdc347f5efd39bf4014ea86077025a8c6d795a82
Maxime Daniel <root@maxux.net>
2019-03-15 13:53:54 +0100

    Cleaning utils file

--- src/utils.c
+++ src/utils.c
@@ -5,7 +5,6 @@
 #include <string.h>
 #include <signal.h>
 
-#ifdef __linux__
 // https://github.com/karelzak/util-linux/blob/master/misc-utils/kill.c
 const char *sys_signame[NSIG] = {
     "zero",  "HUP",  "INT",   "QUIT", "ILL",   "TRAP", "ABRT", "UNUSED",
@@ -13,25 +12,8 @@ const char *sys_signame[NSIG] = {
     "STKFLT","CHLD", "CONT",  "STOP", "TSTP",  "TTIN", "TTOU", "URG",
     "XCPU",  "XFSZ", "VTALRM","PROF", "WINCH", "IO",   "PWR",  "SYS", NULL
 };
-#endif
 
-#if defined(_WIN32) || defined(__CYGWIN__)
-#include <windows.h>
-#include <shellapi.h>
-// https://github.com/mirror/newlib-cygwin/blob/master/winsup/cygwin/strsig.cc
-#ifndef NSIG
-#define NSIG 33
-#endif
-const char *sys_signame[NSIG] = {
-    "zero",  "HUP",  "INT",   "QUIT", "ILL",   "TRAP", "IOT",  "EMT",
-    "FPE",   "KILL", "BUS",   "SEGV", "SYS",   "PIPE", "ALRM", "TERM",
-    "URG",   "STOP", "TSTP",  "CONT", "CHLD",  "TTIN", "TTOU", "IO",
-    "XCPU",  "XFSZ", "VTALRM","PROF", "WINCH", "PWR",  "USR1", "USR2", NULL
-};
-#endif
-
-void *
-xmalloc(size_t size) {
+void *xmalloc(size_t size) {
     if (size == 0)
         return NULL;
     void *p = malloc(size);
@@ -40,8 +22,7 @@ xmalloc(size_t size) {
     return p;
 }
 
-void *
-xrealloc(void *p, size_t size) {
+void *xrealloc(void *p, size_t size) {
     if ((size == 0) && (p == NULL))
         return NULL;
     p = realloc(p, size);
@@ -50,8 +31,7 @@ xrealloc(void *p, size_t size) {
     return p;
 }
 
-char *
-uppercase(char *str) {
+char *uppercase(char *str) {
     int i = 0;
     do {
         str[i] = (char) toupper(str[i]);
@@ -59,22 +39,19 @@ uppercase(char *str) {
     return str;
 }
 
-bool
-endswith(const char *str, const char *suffix) {
+bool endswith(const char *str, const char *suffix) {
     size_t str_len = strlen(str);
     size_t suffix_len = strlen(suffix);
     return str_len > suffix_len && !strcmp(str + (str_len - suffix_len), suffix);
 }
 
-int
-get_sig_name(int sig, char *buf, size_t len) {
+int get_sig_name(int sig, char *buf, size_t len) {
     int n = snprintf(buf, len, "SIG%s", sig < NSIG ? sys_signame[sig] : "unknown");
     uppercase(buf);
     return n;
 }
 
-int
-get_sig(const char *sig_name) {
+int get_sig(const char *sig_name) {
     for (int sig = 1; sig < NSIG; sig++) {
         const char *name = sys_signame[sig];
         if (name != NULL && (strcasecmp(name, sig_name) == 0 || strcasecmp(name, sig_name + 3) == 0))
@@ -84,8 +61,7 @@ get_sig(const char *sig_name) {
 }
 
 // https://github.com/darkk/redsocks/blob/master/base64.c
-char *
-base64_encode(const unsigned char *buffer, size_t length) {
+char *base64_encode(const unsigned char *buffer, size_t length) {
     static const char b64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     char *ret, *dst;
     unsigned i_bits = 0;
