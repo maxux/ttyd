19c5540cddd3cb622b3a0133a87b2f81b71191e4
Maxime Daniel <root@maxux.net>
2019-08-08 15:51:07 +0200

    Update client tfmux to corex

--- client/corex-api-demo.py
+++ client/corex-api-demo.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-class TFMuxExample:
+class CoreXExample:
     def __init__(self, host="localhost", port=7681):
         self.host = host
         self.port = port
@@ -39,10 +39,10 @@ class TFMuxExample:
 
 
 if __name__ == '__main__':
-    tfmux = TFMuxExample()
+    corex = CoreXExample()
 
     print("[+] fetching processes list")
-    plist = tfmux.process_list()
+    plist = corex.process_list()
 
     print("[+] processes found: %d" % len(plist))
     print("[+] ---------------------")
@@ -56,16 +56,16 @@ if __name__ == '__main__':
 
     print("[+]")
     print("[+] cleaning stopped or crashed processes")
-    tfmux.process_clean()
+    corex.process_clean()
 
     print("[+] fetching again processes list")
-    plist = tfmux.process_list()
+    plist = corex.process_list()
 
     print("[+] remaining processes: %d" % len(plist))
     print("[+] ")
 
     print("[+] starting a new short-living process (ls /)")
-    process = tfmux.process_start(["/bin/ls", "/"])
+    process = corex.process_start(["/bin/ls", "/"])
 
     print("[+] process created, id: %d" % process["id"])
     print("[+] waiting a little bit for processes to run...")
@@ -73,7 +73,7 @@ if __name__ == '__main__':
 
     print("[+]")
     print("[+] fetching logs of this process")
-    logs = tfmux.process_logs(process["id"])
+    logs = corex.process_logs(process["id"])
 
     print("[+]")
     print("~~~ BEGIN OF LOGS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
@@ -82,19 +82,19 @@ if __name__ == '__main__':
     print("[+]")
 
     print("[+] stopping all running processes")
-    plist = tfmux.process_list()
+    plist = corex.process_list()
 
     for process in plist:
-        tfmux.process_stop(process["id"])
+        corex.process_stop(process["id"])
 
     print("[+] waiting a little bit for processes to end...")
     time.sleep(2)
 
     print("[+] cleaning stopped or crashed processes")
-    tfmux.process_clean()
+    corex.process_clean()
 
     print("[+] fetching processes list")
-    plist = tfmux.process_list()
+    plist = corex.process_list()
 
     print("[+]")
     print("[+] processes found: %d" % len(plist))
